ef8ddc2aec1dfe0e649b6f0d02f77cdc996b694f
B Karthick <prog@ecomsense.in>
2025-07-18 02:53:03 +0530

    hard code env

--- cron.sh
+++ cron.sh
@@ -1,23 +1,6 @@
 #!/bin/env bash
 
-# Full path to the config file
-CONFIG_FILE="$HOME/.real_user_env"
-
-# Load manually
-if [ -f "$CONFIG_FILE" ]; then
-    source "$CONFIG_FILE"
-fi
-
-# Debug
-{
-    echo "Real user: $REAL_USER"
-} > /tmp/debug.txt 2>&1
-
-printenv > /tmp/env.txt 2>&1
-
-#ACTUAL_HOME=$(getent passwd "$USER" | cut -d: -f6)
 ACTUAL_HOME="$HOME/home/konkakurnool"
-
 
 export PATH="$ACTUAL_HOME/.local/bin:$PATH"
 export PYTHONPATH="$ACTUAL_HOME/.local/lib/python3.11/site-packages"
