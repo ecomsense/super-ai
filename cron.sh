de9781de19b28d9c48c8ca8334491a371626a515
B Karthick <prog@ecomsense.in>
2025-07-17 10:38:17 +0530

    get username from etc

--- cron.sh
+++ cron.sh
@@ -1,11 +1,10 @@
 #!/bin/env bash
 ACTUAL_HOME=$(getent passwd "$USER" | cut -d: -f6)
-new_home="$HOME/$ACTUAL_HOME"
-#source "$new_home/.bashrc"
-export PATH="$new_home/.local/bin:$PATH"
-export PYTHONPATH="$new_home/.local/lib/python3.11/site-packages"
 
-project="$new_home/no_venv/super-ai"
+export PATH="$ACTUAL_HOME/.local/bin:$PATH"
+export PYTHONPATH="$ACTUAL_HOME/.local/lib/python3.11/site-packages"
+
+PROJECT="$ACTUAL_HOME/no_venv/super-ai"
 python3 -c "import sys; print(sys.path)" > /tmp/sys_path_from_cron.txt 2>&1
 
 sess="tmux-session"
@@ -14,7 +13,7 @@ if tmux has-session -t "$sess" 2>/dev/null; then
   exit 0
 fi
 
-cd "$project" || exit 1
-git reset --hard
+cd "$PROJECT" || exit 1
 tmux new-session -d -s "$sess"
-tmux send-keys -t "$sess" "python3 -m src.main && tmux kill-session -t $sess" C-m
+#tmux send-keys -t "$sess" "python3 -m src.main && tmux kill-session -t $sess" C-m
+echo "success"
