63204ae9c4fc3619c2871b1b9a61dc1677be29e7
pannet1 <prog@ecomsense.in>
2026-04-21 08:01:11 +0530

    remove tmux attach from tmux.sh for web UI

--- tmux.sh
+++ tmux.sh
@@ -3,13 +3,11 @@ find . -type d -name "__pycache__" -print0 | xargs -0 rm -rf
 sess="tmux-session"
 
 if tmux has-session -t "$sess" 2>/dev/null; then
-	echo "Session $sess already exists. Attaching to it."
-	tmux attach -t "$sess"
+	echo "Session $sess already exists."
 else
 	echo "updating"
 	git reset --hard && git pull
-	echo "Creating and attaching to session $sess."
+	echo "Creating session $sess."
 	tmux new-session -d -s "$sess"
 	tmux send-keys -t "$sess" ".venv/bin/python -m src.main && tmux kill-session -t $sess" C-m
-	tmux attach -t "$sess"
 fi
