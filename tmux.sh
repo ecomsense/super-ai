43a4244e15565fa139240b05d28b11d03b45b6e3
pannet1 <prog@ecomsense.in>
2026-05-01 22:59:31 +0530

    Revert "revert: restore tmux.sh to web-compatible version (detached mode)"
    This reverts commit e94191e2a7a7cbfd571299691af3318f55c0a4fd.

--- tmux.sh
+++ tmux.sh
@@ -3,11 +3,11 @@ find . -type d -name "__pycache__" -print0 | xargs -0 rm -rf
 sess="tmux-session"
 
 if tmux has-session -t "$sess" 2>/dev/null; then
-	echo "Session $sess already exists."
+	echo "Attaching to session $sess."
+	tmux attach-session -t "$sess"
 else
 	echo "updating"
 	git reset --hard && git pull
-	echo "Creating session $sess."
-	tmux new-session -d -s "$sess"
-	tmux send-keys -t "$sess" ".venv/bin/python -m src.main && tmux kill-session -t $sess" C-m
+	echo "Creating and attaching to session $sess."
+	tmux new-session -s "$sess" ".venv/bin/python -m src.main && tmux kill-session -t $sess"
 fi
